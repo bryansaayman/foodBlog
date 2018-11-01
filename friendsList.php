5e7bb887c057749979a63ca5dd9a984e0856b3dd
bryansaayman <44668927+bryansaayman@users.noreply.github.com>
2018-11-01 13:17:10 +0200

    Add files via upload
    Added comments to the file

--- friendsList.php
+++ friendsList.php
@@ -1,4 +1,8 @@
+<!--This page is used to allow users to add other users as friends. They
+can also search for users to add as friends-->
 <?php
+	//Includes the header php script in the file 
+		//Displays the header content
 	Include "header.php";
 	
 	$action = null;
@@ -10,6 +14,9 @@
 	if(isset($_GET['userid'])){
 		$friendID = $_GET['userid'];
 	}
+	
+	/*If the user clicks the add friend button next to the user, then that user will be added as a friend
+	in the database*/
 	if ($action == "addfriend") {
 		
 		$sql = "SELECT first_name, last_name FROM user_data WHERE id_user = '".$friendID."'";
@@ -34,11 +41,12 @@
 		}
 	}
 ?>
+<!--This is the main content of the page-->
 <div class="container">
 	<div class="row">
 		<div class="col-lg-8 col-md-10 mx-auto">
 			<div class="post-preview"><?php
-				//Displays a success message when a comment is successfully posted
+				//Displays a success message when a user has been added as a friend
 				if ($_SESSION["Success"] != null) {?>
 					<div class="alert alert-success alert-dismissible fade show" role="alert">
 						<?php echo $_SESSION["Success"] ?>
@@ -50,6 +58,7 @@
 				}
 				?>
 			</div>
+			<!--Allows for users to search for other users by typing -->
 			<form method="post" action="friendsList.php?action=search">
 				<fieldset>
 				<div class="form-group">
@@ -60,7 +69,10 @@
 				<button type="submit" class="btn btn-primary" name="Search">Search</button>
 			</form>
 			<br><br><hr>
+			<!--Fetches the data of all users from the database and displays their names along with their status.
+			Each user also has a Add Friend button next to them-->
 			<div class="post-preview">
+				<!--Used to find a user in the database who matches the search-->
 				<?php
 					if ($action == "search"){
 							$search = $_POST["Friend"];
@@ -74,6 +86,7 @@
 					$users = $conn->query($sql);
 					if ($users->num_rows > 0) {
 							while ($row = $users->fetch_assoc()){?>
+								<!--Displays the names and status' of each of the users along with buttons to add them-->
 								<form method="post" action="friendsList.php?action=addfriend&userid=<?php echo $row['id_user'] ?>">
 									<fieldset>
 										<div class="media">
@@ -84,6 +97,7 @@
 												  <input type="hidden" name="hiddenUserID" value="<?php echo $row["id_user"] ?>"/> 
 												</h5>	
 											</div><?php
+											//Checks if the user currently signed in has already added a user as a friend
 											$sql = "SELECT id_user, id_user_friend FROM friends WHERE id_user = '".$ID."' AND id_user_friend = '".$row['id_user']."'";
 											$friend = $conn->query($sql);
 											if ($friend->num_rows <> 0) {?>
@@ -105,5 +119,7 @@
 	</div>
 </div>
 <?php
+	//Includes the footer php script content in the page
+		//Displays the footer content
 	Include "footer.php";
 ?>
